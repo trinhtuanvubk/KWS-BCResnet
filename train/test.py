98e1320b0c4f2f693e9c1e8a77b835629fad8787
trinhtuanvu <vutuantrinh2000@gmail.com>
2021-12-30 09:16:01 +0700

    dev

--- train/test.py
+++ train/test.py
@@ -1,17 +1,16 @@
 
-from torch import nn
 import torch
 from train.args import get_args
 import utils
 def test(args):
     model = utils.get_embedding_model(args)
-    model.print_summary(model)
+    utils.print_summary(model)
     x = torch.randn(3, 101, 40)
     x = model(x)
     print(x.shape)
 
-    model = model.get_model(args)
-    model.print_summary(model)
+    model = utils.get_model(args)
+    utils.print_summary(model)
 
 if __name__=='__main__':
     args = get_args()
